34ee4d4aa02facc184be6acd26f5c465e127c963
NotYetTerminal <61381495+NotYetTerminal@users.noreply.github.com>
2024-03-24 16:37:25 +0000

    Fix spawning

--- Factory/Wall_Gen.gd
+++ Factory/Wall_Gen.gd
@@ -1,18 +1,18 @@
-extends Node
-
 class_name Wall_Gen
+
 
 var Wall: PackedScene = preload("res://scenes/Wall.tscn")
 
 #generates walls
-func generate_walls(array: Array) -> void:
+func generate_walls(caller: Node, array: Array) -> void:
 	var start_position: Vector3 = Vector3(-10,0,10)
 	var test = ""
 	for row in range(array.size()):
 		for col in range(array[0].size()):
 			if array[row][col] == 3:
 				var wall_instance = Wall.instantiate()
-				add_child(wall_instance)
+				caller.add_child(wall_instance)
+				print(wall_instance.get_parent().name)
 				wall_instance.position = start_position + Vector3(col, 0, -row)
 				
 				print(wall_instance)
--- scripts/Game_Loop.gd
+++ scripts/Game_Loop.gd
@@ -9,10 +9,11 @@ var wall_gen = Wall_Gen.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	grid_map.load_rooms()
-	wall_gen.generate_walls(grid_map.get_random_room())
+	wall_gen.generate_walls(self, grid_map.get_random_room())
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+

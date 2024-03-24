e076a4dcfcad961ed6280a944d38d5524b64fd20
NotYetTerminal <61381495+NotYetTerminal@users.noreply.github.com>
2024-03-24 17:46:53 +0000

    Fix warnings

--- scripts/Game_Loop.gd
+++ scripts/Game_Loop.gd
@@ -1,19 +1,11 @@
 extends Node
 
 #global variables (scoping stops us from setting them in _ready)
-var player_actions = 3;
-var player_health = 3;
-var grid_map = Grid_Map.new()
-var wall_gen = Wall_Gen.new()
+var player_actions: int = 3;
+var player_health: int = 3;
+var grid_map: Grid_Map = Grid_Map.new()
+var wall_gen: Wall_Gen = Wall_Gen.new()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	grid_map.load_rooms()
 	wall_gen.generate_walls($Walls, grid_map.get_random_room())
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-

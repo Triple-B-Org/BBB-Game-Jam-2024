d7c5b5a359b22931e37d382ff3e820ba628e3525
NotYetTerminal <61381495+NotYetTerminal@users.noreply.github.com>
2024-03-24 19:23:29 +0000

    Resolution and full screen

--- scripts/Settings.gd
+++ scripts/Settings.gd
@@ -2,7 +2,11 @@ extends Node
 
 
 var smooth_movement: bool = true
+var move_speed: float = 5.0
 
+
+var fullscreen: bool = false
+var resolution_index: int = 4
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_WM_CLOSE_REQUEST:

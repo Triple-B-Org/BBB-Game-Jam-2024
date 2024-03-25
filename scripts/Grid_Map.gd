ccf1a03c42d96f6bc643cd9687bfa3d0a145ba3b
KelanMorgan <kelanmorgan2013@gmail.com>
2024-03-25 18:11:09 +0000

    Added movement in the array and made the grid access global

--- scripts/Grid_Map.gd
+++ scripts/Grid_Map.gd
@@ -2,17 +2,10 @@ class_name Grid_Map
 
 #THIS IS ONLY FOR TESTING FOR N0W, NEEDS TO BE MADE DINAMIC, ADDED TO MY LIST (KELAN)
 var Rooms: Array = []
-var Current_Room: Array = []
-
-func _ready() -> void:
-	load_rooms()
 	
 # Returns a random room to be generated
-func get_random_room() -> Array:
-	Current_Room = Rooms[randi() % Rooms.size()]
-	return Current_Room
-
-
+func get_random_room() -> void:
+	GlobalVar.Current_Room = Rooms[randi() % Rooms.size()]
 
 #this function is for putting in your room layouts
 func load_rooms() -> void:

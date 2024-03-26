b4ba8b2aeba9df0492ef7555b19f3ac0c7e8bfc1
KelanMorgan <kelanmorgan2013@gmail.com>
2024-03-26 22:49:09 +0000

    added turns

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -30,32 +30,98 @@ var footsteps: AudioStreamWAV = preload("res://assets/footsteps.wav")
 var reverseFootsteps: AudioStreamWAV = preload("res://assets/footstepsReverse.wav")
 
 func _physics_process(delta: float) -> void:
-	if Settings.smooth_movement:
-		if t_movement >= 1 and t_rotation >= 1:
+	if GlobalVar.players_turn == 2:
+		if Settings.smooth_movement:
+			if t_movement >= 1 and t_rotation >= 1:
+				if Input.is_action_just_pressed("move_forward"):
+					if check_move_forward() == true:
+						start_position = position
+						end_position = start_position - Vector3(direction.x, 0, direction.y)
+						t_movement = 0
+						audio_player.stream = footsteps
+						audio_player.play()
+						
+						GlobalVar.player_actions -= 1
+						if GlobalVar.player_actions == 0:
+							GlobalVar.players_turn = 0
+							GlobalVar.enemy_turn = 1
+						
+				elif Input.is_action_just_pressed("move_backward"):
+					if check_move_backward() == true:
+						start_position = position
+						end_position = start_position + Vector3(direction.x, 0, direction.y)
+						t_movement = 0
+						audio_player.stream = reverseFootsteps
+						audio_player.play()
+						
+						GlobalVar.player_actions -= 1
+						if GlobalVar.player_actions == 0:
+							GlobalVar.players_turn = 0
+							GlobalVar.enemy_turn = 1
+						
+				elif Input.is_action_just_pressed("turn_right"):
+					start_rotation = rotation.y
+					end_rotation = start_rotation - PI/2
+					direction = Vector2(sin(end_rotation), cos(end_rotation))
+					t_rotation = 0
+					
+					if facing == "N":
+						facing = "E"
+					elif facing == "E":
+						facing = "S"
+					elif facing == "S":
+						facing = "W"
+					else:
+						facing = "N"
+				elif Input.is_action_just_pressed("turn_left"):
+					start_rotation = rotation.y
+					end_rotation = start_rotation + PI/2
+					direction = Vector2(sin(end_rotation), cos(end_rotation))
+					t_rotation = 0
+					
+					if facing == "N":
+						facing = "W"
+					elif facing == "E":
+						facing = "N"
+					elif facing == "S":
+						facing = "E"
+					else:
+						facing = "S"
+			# only rotation or movement is allowed at a time
+			elif t_rotation >= 1:
+				t_movement += SPEED * delta
+				position = start_position.lerp(end_position, t_movement)
+			elif t_movement >= 1:
+				t_rotation += SPEED * delta
+				rotation.y = lerp(start_rotation, end_rotation, t_rotation)
+			# however if for some reason both are activated they are both moved to their end positions
+			else:
+				t_movement = 1
+				position = start_position.lerp(end_position, t_movement)
+				t_rotation = 1
+				rotation.y = lerp(start_rotation, end_rotation, t_rotation)
+		else:
 			if Input.is_action_just_pressed("move_forward"):
 				if check_move_forward() == true:
-					start_position = position
-					end_position = start_position - Vector3(direction.x, 0, direction.y)
-					t_movement = 0
-					audio_player.stream = footsteps
-					audio_player.play()
-					
+					position.x -= direction.x
+					position.z -= direction.y
+					GlobalVar.player_actions -= 1
+					if GlobalVar.player_actions == 0:
+						GlobalVar.players_turn = 0
+						GlobalVar.enemy_turn = 1
 			elif Input.is_action_just_pressed("move_backward"):
 				if check_move_backward() == true:
-					start_position = position
-					end_position = start_position + Vector3(direction.x, 0, direction.y)
-					t_movement = 0
-					audio_player.stream = reverseFootsteps
-					audio_player.play()
-					
+					position.x += direction.x
+					position.z += direction.y
+					GlobalVar.player_actions -= 1
+					if GlobalVar.player_actions == 0:
+						GlobalVar.players_turn = 0
+						GlobalVar.enemy_turn = 1
 			elif Input.is_action_just_pressed("turn_right"):
-				start_rotation = rotation.y
-				end_rotation = start_rotation - PI/2
-				direction = Vector2(sin(end_rotation), cos(end_rotation))
-				t_rotation = 0
-				
+				rotation.y -= PI/2
+				direction = Vector2(sin(rotation.y), cos(rotation.y))
 				if facing == "N":
-					facing = "E"
+						facing = "E"
 				elif facing == "E":
 					facing = "S"
 				elif facing == "S":
@@ -63,11 +129,8 @@ func _physics_process(delta: float) -> void:
 				else:
 					facing = "N"
 			elif Input.is_action_just_pressed("turn_left"):
-				start_rotation = rotation.y
-				end_rotation = start_rotation + PI/2
-				direction = Vector2(sin(end_rotation), cos(end_rotation))
-				t_rotation = 0
-				
+				rotation.y += PI/2
+				direction = Vector2(sin(rotation.y), cos(rotation.y))
 				if facing == "N":
 					facing = "W"
 				elif facing == "E":
@@ -76,50 +139,6 @@ func _physics_process(delta: float) -> void:
 					facing = "E"
 				else:
 					facing = "S"
-		# only rotation or movement is allowed at a time
-		elif t_rotation >= 1:
-			t_movement += SPEED * delta
-			position = start_position.lerp(end_position, t_movement)
-		elif t_movement >= 1:
-			t_rotation += SPEED * delta
-			rotation.y = lerp(start_rotation, end_rotation, t_rotation)
-		# however if for some reason both are activated they are both moved to their end positions
-		else:
-			t_movement = 1
-			position = start_position.lerp(end_position, t_movement)
-			t_rotation = 1
-			rotation.y = lerp(start_rotation, end_rotation, t_rotation)
-	else:
-		if Input.is_action_just_pressed("move_forward"):
-			if check_move_forward() == true:
-				position.x -= direction.x
-				position.z -= direction.y
-		elif Input.is_action_just_pressed("move_backward"):
-			if check_move_backward() == true:
-				position.x += direction.x
-				position.z += direction.y
-		elif Input.is_action_just_pressed("turn_right"):
-			rotation.y -= PI/2
-			direction = Vector2(sin(rotation.y), cos(rotation.y))
-			if facing == "N":
-					facing = "E"
-			elif facing == "E":
-				facing = "S"
-			elif facing == "S":
-				facing = "W"
-			else:
-				facing = "N"
-		elif Input.is_action_just_pressed("turn_left"):
-			rotation.y += PI/2
-			direction = Vector2(sin(rotation.y), cos(rotation.y))
-			if facing == "N":
-				facing = "W"
-			elif facing == "E":
-				facing = "N"
-			elif facing == "S":
-				facing = "E"
-			else:
-				facing = "S"
 
 
 func check_move_forward() -> bool:

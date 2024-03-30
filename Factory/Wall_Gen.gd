c0869c4c13c7499edc43cf3435aaea4aa19da75e
NotYetTerminal <61381495+NotYetTerminal@users.noreply.github.com>
2024-03-30 22:59:30 +0000

    Walls spawning

--- Factory/Wall_Gen.gd
+++ Factory/Wall_Gen.gd
@@ -1,7 +1,7 @@
 class_name Wall_Gen
 
 
-var Wall: PackedScene = preload("res://scenes/Wall.tscn")
+var Wall: PackedScene = preload("res://scenes/Environment/Wall.tscn")
 
 #generates walls
 func generate_walls(parent: Node) -> void:
@@ -11,12 +11,26 @@ func generate_walls(parent: Node) -> void:
 	for row: int in range(array.size()):
 		for col: int in range(one_row.size()):
 			if array[row][col] == 3:
-				var wall_instance: MeshInstance3D = Wall.instantiate()
+				var wall_instance: Node3D = Wall.instantiate()
 				parent.add_child(wall_instance)
 				wall_instance.position = start_position + Vector3(col, 0, row)
+				wall_instance.set_mesh_and_objects(pack_neighbour_data(row, col, array.size(), one_row.size()))
 
 
 func unload_walls(parent: Node) -> void:
 	for n: Node in parent.get_children():
 		parent.remove_child(n)
 		n.queue_free()
+
+
+func pack_neighbour_data(row: int, col: int, max_row: int, max_col: int) -> Vector4i:
+	var neighbours: Vector4i = Vector4i.ONE
+	if row != 0:
+		neighbours.x = int(GlobalVar.Current_Room[row - 1][col] == 3)
+	if row != max_row - 1:
+		neighbours.y = int(GlobalVar.Current_Room[row + 1][col] == 3)
+	if col != 0:
+		neighbours.z = int(GlobalVar.Current_Room[row][col - 1] == 3)
+	if col != max_col - 1:
+		neighbours.w = int(GlobalVar.Current_Room[row][col + 1] == 3)
+	return neighbours
--- scripts/Environment/Wall.gd
+++ scripts/Environment/Wall.gd
@@ -1,5 +1,29 @@
-extends MeshInstance3D
+extends Node3D
+
+
+@onready var wall_plane: PackedScene = preload("res://scenes/Environment/WallPlane.tscn")
+@onready var pot: PackedScene = preload("res://scenes/Environment/Pot.tscn")
+@onready var suzanne: PackedScene = preload("res://scenes/Environment/Suzanne.tscn")
+@onready var sword: PackedScene = preload("res://scenes/Environment/Sword.tscn")
+@onready var torch: PackedScene = preload("res://scenes/Environment/Torch.tscn")
 
 
 func set_mesh_and_objects(neighbours: Vector4i) -> void:
-	pass
+	var wall_plane_instance: MeshInstance3D
+	if neighbours.x != 1:
+		wall_plane_instance = wall_plane.instantiate()
+		add_child(wall_plane_instance)
+		wall_plane_instance.rotation.y = PI / 2
+	if neighbours.y != 1:
+		wall_plane_instance = wall_plane.instantiate()
+		add_child(wall_plane_instance)
+		wall_plane_instance.rotation.y = PI * 1.5
+	if neighbours.z != 1:
+		wall_plane_instance = wall_plane.instantiate()
+		add_child(wall_plane_instance)
+		wall_plane_instance.rotation.y = PI
+	if neighbours.w != 1:
+		wall_plane_instance = wall_plane.instantiate()
+		add_child(wall_plane_instance)
+	
+	

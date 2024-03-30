0aa126e1ed0932495f66497d3ebd65d27f1b7b3e
NotYetTerminal <61381495+NotYetTerminal@users.noreply.github.com>
2024-03-30 20:46:40 +0000

    Enemy bobbing

--- scripts/Enemy_rotation.gd
+++ scripts/Enemy_rotation.gd
@@ -3,7 +3,11 @@ extends Node3D
 @onready var player: Node3D = $"../../Player"
 @export var enemy: MeshInstance3D
 
+var time: float = 0
+
+
 func _process(_delta: float) -> void:
 	enemy.rotation.y = -atan2(position.z - player.position.z, position.x - player.position.x)
-	
-	
+	time += 0.05
+	position.y = (sin(time) / 7 + 0.9)
+

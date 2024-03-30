1b845a38e18b33262e4ce59988451aed47bcd987
NotYetTerminal <61381495+NotYetTerminal@users.noreply.github.com>
2024-03-30 23:19:05 +0000

    Items spawn

--- scripts/Environment/Wall.gd
+++ scripts/Environment/Wall.gd
@@ -2,28 +2,25 @@ extends Node3D
 
 
 @onready var wall_plane: PackedScene = preload("res://scenes/Environment/WallPlane.tscn")
-@onready var pot: PackedScene = preload("res://scenes/Environment/Pot.tscn")
-@onready var suzanne: PackedScene = preload("res://scenes/Environment/Suzanne.tscn")
-@onready var sword: PackedScene = preload("res://scenes/Environment/Sword.tscn")
-@onready var torch: PackedScene = preload("res://scenes/Environment/Torch.tscn")
+
+@export var scattered_items: Array[PackedScene]
 
 
 func set_mesh_and_objects(neighbours: Vector4i) -> void:
-	var wall_plane_instance: MeshInstance3D
 	if neighbours.x != 1:
-		wall_plane_instance = wall_plane.instantiate()
-		add_child(wall_plane_instance)
-		wall_plane_instance.rotation.y = PI / 2
+		spawn_wall(PI / 2)
 	if neighbours.y != 1:
-		wall_plane_instance = wall_plane.instantiate()
-		add_child(wall_plane_instance)
-		wall_plane_instance.rotation.y = PI * 1.5
+		spawn_wall(PI * 1.5)
 	if neighbours.z != 1:
-		wall_plane_instance = wall_plane.instantiate()
-		add_child(wall_plane_instance)
-		wall_plane_instance.rotation.y = PI
+		spawn_wall(PI)
 	if neighbours.w != 1:
-		wall_plane_instance = wall_plane.instantiate()
-		add_child(wall_plane_instance)
-	
-	
+		spawn_wall(0)
+
+
+func spawn_wall(rotation_radian: float) -> void:
+	var wall_plane_instance: MeshInstance3D = wall_plane.instantiate()
+	add_child(wall_plane_instance)
+	if randi() % 4 == 0:
+		var spawn_item: MeshInstance3D = scattered_items[randi_range(0, scattered_items.size() - 1)].instantiate()
+		wall_plane_instance.add_child(spawn_item)
+	wall_plane_instance.rotation.y = rotation_radian

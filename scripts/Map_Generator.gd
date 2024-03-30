567bf2c3c4eb716e63b3ae9da8bd1757bfa0aee2
Adam Fitzpatrick <113912436+adam-fitz395@users.noreply.github.com>
2024-03-30 20:31:40 +0000

    Map now functional && updated graphics (kinda)

--- scripts/Map_Generator.gd
+++ scripts/Map_Generator.gd
@@ -1,9 +1,6 @@
 extends Node
 class_name Map_Gen
 
-var num_rooms_visited: int = 0
-signal icon_change
-
 func _ready() -> void:
 	initialize_map()
 	pass
@@ -11,17 +8,17 @@ func _ready() -> void:
 func initialize_map() -> void:
 	GlobalVar.selectable_rooms = []
 	# Define the weights for choosing fights and rests
-	while GlobalVar.selectable_rooms.size() != 3:
-		if num_rooms_visited % 5 == 0 && num_rooms_visited != 0:
-			GlobalVar.selectable_rooms.append(3)
-		var fight_weight: int = 3  # Adjust this value to increase or decrease the likelihood of picking fight
-		var rest_weight: int = 1  # Adjust this value to increase or decrease the likelihood of picking rest
-		var random_room_value: int = randi() % (fight_weight + rest_weight)
-		if random_room_value < fight_weight:
-			GlobalVar.selectable_rooms.append(1)
+	while GlobalVar.selectable_rooms.size() < 3:
+		if GlobalVar.num_rooms_visited % 5 == 0:
+				GlobalVar.selectable_rooms.append(3)
 		else:
-			GlobalVar.selectable_rooms.append(2)
-	emit_signal("icon_change")
+			var fight_weight: int = 3  # Adjust this value to increase or decrease the likelihood of picking fight
+			var rest_weight: int = 1  # Adjust this value to increase or decrease the likelihood of picking rest
+			var random_room_value: int = randi() % (fight_weight + rest_weight)
+			if random_room_value < fight_weight:
+				GlobalVar.selectable_rooms.append(1)
+			else:
+				GlobalVar.selectable_rooms.append(2)
 	
 	print(GlobalVar.selectable_rooms)
 
@@ -32,12 +29,18 @@ func reset_selectable_rooms() -> void:
 func get_left_option() -> int:
 	print(GlobalVar.selectable_rooms)
 	var left_room: int = GlobalVar.selectable_rooms[0]
+	GlobalVar.num_rooms_visited += 1
+	print(GlobalVar.num_rooms_visited)
 	return left_room
 
 func get_middle_option() -> int:
 	var middle_room: int = GlobalVar.selectable_rooms[1]
+	GlobalVar.num_rooms_visited += 1
+	print(GlobalVar.num_rooms_visited)
 	return middle_room
 	
 func get_right_option() -> int:
 	var right_room: int = GlobalVar.selectable_rooms[2]
+	GlobalVar.num_rooms_visited += 1
+	print(GlobalVar.num_rooms_visited)
 	return right_room
